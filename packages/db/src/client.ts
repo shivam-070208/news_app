a6b8cd2841acbf2fb231b3dc6e6a4f942dab8019
shvgpt-070208 <shvgpt070208@gmail.com>
2026-04-16 22:48:38 +0530

    feat: created db workspace

---
+++ packages/db/src/client.ts
@@ -0,0 +1,24 @@
+import "dotenv/config"
+import { PrismaPg } from "@prisma/adapter-pg"
+
+import { Pool } from "pg"
+import { PrismaClient } from "./generated/prisma/client"
+
+const connectionString = process.env.DATABASE_URL
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient }
+const db =
+  globalForPrisma.prisma ??
+  (() => {
+    const pool = new Pool({ connectionString })
+    const adapter = new PrismaPg(pool)
+    return new PrismaClient({ adapter })
+  })()
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db
+}
+export { db }
