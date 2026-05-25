91ca98be4e79a51306385539f2b7026066f2232a
ayushy738 <a4ayushyadav2007@gmail.com>
2026-05-26 04:19:27 +0530

    fixed import errors

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,9 +2,12 @@ import "dotenv/config"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 import { Pool } from "pg"
-import { PrismaClient } from "./generated/prisma/client.ts"
+import { PrismaClient } from "./generated/prisma/client"
 
 const connectionString = process.env.DATABASE_URL
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is required")
+}
 declare global {
   var prisma: PrismaClient | undefined
 }
@@ -13,6 +16,9 @@ const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient }
 const db =
   globalForPrisma.prisma ??
   (() => {
+    if (!connectionString) {
+      throw new Error("DATABASE_URL environment variable is required")
+    }
     const pool = new Pool({ connectionString })
     const adapter = new PrismaPg(pool)
     return new PrismaClient({ adapter })
